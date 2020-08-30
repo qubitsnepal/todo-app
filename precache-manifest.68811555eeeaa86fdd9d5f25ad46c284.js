399b1f4498d4c5eee4043028c3a851c9a1e19f1d
Sagar Gurung <sagargurung@pop-os.localdomain>
2020-08-30 08:59:38 +0545

    Updates

---
+++ precache-manifest.68811555eeeaa86fdd9d5f25ad46c284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce7f16d4f03ae2b9b1bbf55b4e83124",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d78af7703f1c5b027284",
+    "url": "/static/css/main.1e0f519f.chunk.css"
+  },
+  {
+    "revision": "1c5c64f55cc9cb4342a4",
+    "url": "/static/js/2.242398fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.242398fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78af7703f1c5b027284",
+    "url": "/static/js/main.77014ff8.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
